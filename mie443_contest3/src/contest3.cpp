3c51e5c613a94fe9049e18e3b12a7dc47290cadb
DSCod3 <dhruvsharmaone@gmail.com>
2025-03-25 17:51:02 -0400

    Updated with percy addition and my wav files
    On branch dhruv_c3
    Your branch is up to date with 'origin/dhruv_c3'.
    
    Changes to be committed:
    	modified:   mie443_contest3/src/contest3.cpp

--- mie443_contest3/src/contest3.cpp
+++ mie443_contest3/src/contest3.cpp
@@ -11,22 +11,20 @@ geometry_msgs::Twist follow_cmd;
 int world_state;
 Status status;
 bool playingSound;
-ros::Time lastFollowTime;  // To track the last follower command
 
-// Update follower callback to refresh the tracking timer.
 void followerCB(const geometry_msgs::Twist msg){
     follow_cmd = msg;
-    lastFollowTime = ros::Time::now(); // Update last received time
 }
 
 void setMovement(geometry_msgs::Twist &vel, ros::Publisher &vel_pub, float lx, float rz){
-	vel.angular.z = rz;
-	vel.linear.x = lx;
-	vel_pub.publish(vel);
-	ros::spinOnce();
+    vel.angular.z = rz;
+    vel.linear.x = lx;
+    vel_pub.publish(vel);
+    ros::spinOnce();
 }
 
 //-------------------------------------------------------------
+
 int main(int argc, char **argv)
 {
     status = S_FOLLOW;
@@ -38,7 +36,7 @@ int main(int argc, char **argv)
     teleController eStop;
 
     // Publishers
-    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop",1);
+    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
     // Subscribers
     ros::Subscriber follower = nh.subscribe("follower_velocity_smoother/smooth_cmd_vel", 10, &followerCB);
@@ -52,10 +50,12 @@ int main(int argc, char **argv)
     uint64_t secondsElapsed = 0;
     uint64_t timeReference = 0;
 
-    imageTransporter rgbTransport("camera/image/", sensor_msgs::image_encodings::BGR8);
+    imageTransporter rgbTransport("camera/image/", sensor_msgs::image_encodings::BGR8);  //-- for Webcam
+    // imageTransporter rgbTransport("camera/rgb/image_raw", sensor_msgs::image_encodings::BGR8);  //-- for turtlebot Camera
     imageTransporter depthTransport("camera/depth_registered/image_raw", sensor_msgs::image_encodings::TYPE_32FC1);
 
     int world_state = 0;
+
     double angular = 0.2;
     double linear = 0.0;
 
@@ -63,87 +63,114 @@ int main(int argc, char **argv)
     vel.angular.z = angular;
     vel.linear.x = linear;
 
+    // sc.playWave(path_to_sounds + "sound.wav");
     ros::Duration(0.5).sleep();
 
-    while(ros::ok() && secondsElapsed <= 480){		
+    while(ros::ok() && secondsElapsed <= 480){
         ros::spinOnce();
-        ros::Time current_time = ros::Time::now();
-        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(current_time - start).count();
-
-        // LOST TRACKING CHECK:
-        // If in S_FOLLOW and no follower command received for >3 seconds, switch to lost tracking (S_PLACEHOLDER)
-        if(status == S_FOLLOW && (current_time - lastFollowTime).toSec() > 3.0){
-            status = S_PLACEHOLDER;
-        }
-        // If in lost tracking and a recent follower command is received, revert to follow.
-        if(status == S_PLACEHOLDER && (current_time - lastFollowTime).toSec() < 1.0){
-            status = S_FOLLOW;
-            playingSound = false; // reset sound flag
-        }
 
         switch(status){
             case S_FOLLOW:
+                ROS_INFO("S_FOLLOW");
                 playingSound = false;
                 vel_pub.publish(follow_cmd);
                 break;
 
             case S_BUMPER:
                 ROS_INFO("BUMPER PRESSED EVENT");
+
                 if(!playingSound){
                     playingSound = true;
                     sc.playWave(path_to_sounds + "Rage.wav");
                 }
+
+                // Move backwards for 2 seconds.
                 timeReference = secondsElapsed;
                 while(secondsElapsed - timeReference < 2){
-                    ros::spinOnce();
-                    secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(ros::Time::now() - start).count();
+                    secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(
+                        std::chrono::system_clock::now()-start).count();
                     setMovement(vel, vel_pub, -0.2, 0);
                 }
+
+                // After backing up, perform a shaking behavior.
+                // Shake left-right for a few iterations.
+                for (int i = 0; i < 3; i++){
+                    // Shake left: turn right (positive angular velocity)
+                    timeReference = secondsElapsed;
+                    while(secondsElapsed - timeReference < 1){
+                        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(
+                            std::chrono::system_clock::now()-start).count();
+                        setMovement(vel, vel_pub, 0, 0.5);
+                    }
+                    // Shake right: turn left (negative angular velocity)
+                    timeReference = secondsElapsed;
+                    while(secondsElapsed - timeReference < 1){
+                        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(
+                            std::chrono::system_clock::now()-start).count();
+                        setMovement(vel, vel_pub, 0, -0.5);
+                    }
+                }
+
+                // Stop movement after the bumper reaction.
                 setMovement(vel, vel_pub, 0, 0);
                 ros::spinOnce();
-                status = S_FOLLOW;  // Revert to follow after bumper event
-                playingSound = false;
                 break;
 
             case S_CLIFF:
                 ROS_INFO("CLIFF ACTIVE EVENT");
                 setMovement(vel, vel_pub, 0, 0);
+
+                // Cliff sensor needs to be debounced.
+                timeReference = secondsElapsed;
+                while(secondsElapsed - timeReference < 1){
+                    if(status == S_CLIFF){
+                        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(
+                            std::chrono::system_clock::now()-start).count();
+                        timeReference = secondsElapsed;
+                    }
+                    ros::spinOnce();
+                }
+
                 if(!playingSound){
                     playingSound = true;
                     sc.playWave(path_to_sounds + "Discontent.wav");
                 }
-                // When cliff is no longer active, revert to follow.
-                if(!cliffActive){
-                    status = S_FOLLOW;
-                    playingSound = false;
-                }
                 break;
 
             case S_MICROPHONE:
-                ROS_INFO("MICROPHONE EVENT: INFATUATED");
                 setMovement(vel, vel_pub, 0, 0);
-                if(!playingSound){
-                    playingSound = true;
-                    sc.playWave(path_to_sounds + "Infatuated.wav");
-                }
-                // After processing microphone input, revert to follow.
-                status = S_FOLLOW;
-                playingSound = false;
                 break;
 
-            case S_PLACEHOLDER:  // LOST TRACKING state
-                ROS_INFO("LOST TRACKING: SEARCHING");
-                if(!playingSound){
-                    playingSound = true;
-                    sc.playWave(path_to_sounds + "sad.wav");
-                }
-                // Search behavior: rotate slowly in place to try to find the person.
-                setMovement(vel, vel_pub, 0, 0.5);
+            case S_PLACEHOLDER:
+                setMovement(vel, vel_pub, 0, 0);
                 break;
         }
 
+		// if(cliffActive){
+			
+		// }
+
+		// if(bumpers.anyPressed){
+			
+		// }
+
+
+		// if(world_state == 0){
+		// 	//fill with your code
+		// 	//vel_pub.publish(vel);
+		// 	vel_pub.publish(follow_cmd);
+
+		// }else if(world_state == 1){
+		// 	/*
+		// 	...
+		// 	...
+		// 	*/
+		// }
+
+		secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(
+            std::chrono::system_clock::now()-start).count();
         loop_rate.sleep();
     }
-
-    return 0;
+	
+	return 0;
 }

6efddbc124de48ab1e06feaa1a563d8364bd6016
HarryGN <haoran.guan@mail.utoronto.ca>
2025-03-29 14:42:52 -0400

    changed angry emoji

--- mie443_contest3/src/contest3.cpp
+++ mie443_contest3/src/contest3.cpp
@@ -189,7 +189,7 @@ int main(int argc, char **argv)
 				if(!playingSound){
 					playingSound = true;
 					sc.playWave(path_to_sounds + "Angry.wav");
-					showEmojiFullscreen("angry_emoji.png", 2000);
+					showEmojiFullscreen("very_angry_emoji.png", 2000);
 				}
 
 				timeReference = secondsElapsed;
